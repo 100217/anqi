e513fb3dc67ebe33bd9302eeeb75409b258ad257
qiuyu.zhuang <qiuyu.zhuang@getech.cn>
2020-12-10 14:26:52 +0800

    初始化项目，添加初始依赖

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav>安琪会员中心</nav>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+  'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+  'Noto Color Emoji';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+nav{
+  height: 2rem;
+  line-height: 2;
+  text-align: center;
+  border-bottom: 1px solid #ddd;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="primary">主要按钮</van-button>
+    <img :src="src" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import img from '../assets/logo.png'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    
+  },
+  data() {
+    return {
+      src: img,
+    }
+  },
 }
 </script>
